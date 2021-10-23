05dfeca9365b732d625a4ad6852a481baa7cc5e4
rohansethi87 <rohan.sethi87@gmail.com>
2021-10-23 20:59:24 +0530

    Cart Component WIP

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
-import { Container, Grid, Typography, Button, Grid} from '@material-ui/core';
+import { Container, Grid, Typography, Button} from '@material-ui/core';
 
-const Cart = () => {
+const Cart = ({ cart }) => {
+    console.log(cart.line_items.length);
+    // const isEmpty = cart.line_items.length;
+    // const isEmpty = true;
+
+    const EmptyCart = () => {
+        <Typography variant="subtitle1">You have no items in your shopping cart, start adding some!</Typography>
+    };
+
+    const FilledCart = () => {
+        <>
+            <Grid container spacing={3}>
+
+            </Grid>
+        </>
+    }
+
     return (
         <Container>
-            <div className="classes.toolbar"></div>
+            <div className="classes.toolbar" />
+            <Typography className="classes.title" variant="h3">Your Shopping Cart</Typography>
+            {/* {isEmpty ? <EmptyCart />: <FilledCart />} */}
         </Container>
     )
 }
